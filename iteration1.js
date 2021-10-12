b0805124aa259d5993df4af719addfca4e6abfdd
ZEEKER <zeeker@MacBook-Air-de-ZEEKER.local>
2021-10-12 23:47:13 +0200

    events

---
+++ iteration1.js
@@ -0,0 +1,32 @@
+/* 1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el 
+evento click que ejecute un console log con la información del evento del click
+
+1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
+
+1.3 Añade un evento 'input' que ejecute un console.log con el valor del input. */
+
+
+     
+//1.1
+const btnClick = (event) => {
+    console.log(event)
+}
+document.getElementById("btnToClick").addEventListener("click", btnClick);
+
+
+//1.2
+const btnFocus = (event) => {
+    console.log(event);
+}
+document.getElementById("btnToClick").addEventListener("focus", btnFocus);
+
+
+//1.3
+
+const input = document.querySelector(".value");
+
+    input.addEventListener("input", () => {
+        console.log(input.value);
+    
+    })
+
